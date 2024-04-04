c118141fb1c24d7db99bb5fc87044dadf52e13d4
Booker Ngoon <bookerngoon@gmail.com>
2024-04-04 17:31:05 -0400

    last part

--- main_app/urls.py
+++ main_app/urls.py
@@ -12,10 +12,10 @@ urlpatterns = [
     path('characters/<int:character_id>/conditions/<int:id>/',
          ConditionDetail.as_view(), name='condition-detail'),
 
-    path('characters/int:character_id>/add_accessory/int:accessory_id>/',
+    path('characters/<int:character_id>/add_accessory/<int:accessory_id>/',
          AddAccessoryToCharacter.as_view(), name='add-accessory-to-character'),
 
-    path('characters/int:character_id>/remove_accessory/int:accessory_id>/',
+    path('characters/<int:character_id>/remove_accessory/<int:accessory_id>/',
          RemoveAccessoryFromCharacter.as_view(), name='remove-accessory-from-character'),
 
     path('users/register/', CreateUserView.as_view(), name='register'),
