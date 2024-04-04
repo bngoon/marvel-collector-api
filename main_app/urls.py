75ecebbb591640ad7020bb27252a4d53385cf961
Booker Ngoon <bookerngoon@gmail.com>
2024-04-04 11:54:26 -0400

    remove accessory completed

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Home, CharacterList, CharacterDetail, ConditionListCreate, ConditionDetail
+from .views import Home, CharacterList, CharacterDetail, ConditionListCreate, ConditionDetail, AddAccessoryToCharacter, RemoveAccessoryFromCharacter
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
@@ -8,6 +8,13 @@ urlpatterns = [
     path('characters/<int:id>/', CharacterDetail.as_view(), name='character-detail'),
     path('characters/<int:character_id>/conditions/',
          ConditionListCreate.as_view(), name='condition-list-create'),
+
     path('characters/<int:character_id>/conditions/<int:id>/',
-         ConditionDetail.as_view(), name='condition-detail')
+         ConditionDetail.as_view(), name='condition-detail'),
+
+    path('characters/int:character_id>/add_accessory/int:accessory_id>/',
+         AddAccessoryToCharacter.as_view(), name='add-accessory-to-character'),
+
+    path('characters/int:character_id>/remove_accessory/int:accessory_id>/',
+         RemoveAccessoryFromCharacter.as_view(), name='remove-accessory-from-character')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,8 +3,8 @@ from rest_framework.response import Response
 
 
 from rest_framework import generics
-from .models import Character, Condition
-from .serializers import CharacterSerializer, ConditionSerializer
+from .models import Character, Condition, Accessory
+from .serializers import CharacterSerializer, ConditionSerializer, AccessorySerializer
 # Define the home view
 
 
@@ -23,6 +23,20 @@ class CharacterDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Character.objects.all()
     serializer_class = CharacterSerializer
     lookup_field = 'id'
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+
+        accessories_not_associated = Accessory.objects.exclude(
+            id__in=instance.toys.all())
+        accessories_serializer = AccessorySerializer(
+            accessories_not_associated, many=True)
+
+        return Response({
+            'character': serializer.data,
+            'accessories_not_associated': accessories_serializer.data
+        })
 
 
 class ConditionListCreate(generics.ListCreateAPIView):
@@ -45,3 +59,19 @@ class ConditionDetail(generics.RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         character_id = self.kwargs['cracter_id']
         return Condition.objects.filter(character_id=character_id)
+
+
+class AddAccessoryToCharacter(APIView):
+    def post(self, request, character_id, accessory_id):
+        character = Character.objects.get(id=character_id)
+        accessory = Accessory.objects.get(id=accessory_id)
+        character.accessories.add(accessory)
+        return Response({'message': f'Accessory {accessory.name} added to Character {character.name}'})
+
+
+class RemoveAccessoryFromCharacter(APIView):
+    def post(self, request, character_id, accessory_id):
+        character = Character.objects.get(id=character_id)
+        accessory = Accessory.objects.get(id=accessory_id)
+        character.accessories.add(accessory)
+        return Response({'message': f'Accessory {accessory.name} taken away from Character {character.name}'})
